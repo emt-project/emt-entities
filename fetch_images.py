afc6888dc481b46c669d905cc61bdcae3698d47d
csae8092 <p.andorfer@gmail.com>
2025-09-23 14:04:44 +0200

    fix: adds missing request header for wikidata api call

--- fetch_images.py
+++ fetch_images.py
@@ -5,7 +5,9 @@ from AcdhArcheAssets.uri_norm_rules import get_norm_id
 
 current_table = br_client.get_table_by_name(BASEROW_DB_ID, "persons")
 WIKIDATA_EP = "https://www.wikidata.org/w/api.php?action=wbgetclaims&property=P18&entity={}&format=json"
-
+DEFAULT_REQUEST_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0"
+}
 
 items = []
 filters = {"filter__field_24994__contains": "http", "filter__field_24997__empty": True}
@@ -18,7 +20,7 @@ for x in tqdm(items):
     update_url = f"{br_client.br_base_url}database/rows/table/{current_table}/{x['id']}/?user_field_names=true"
     wd_id = get_norm_id(x["wikidata_url"])
     url = WIKIDATA_EP.format(wd_id)
-    r = requests.get(url)
+    r = requests.get(url, headers=DEFAULT_REQUEST_HEADERS)
     try:
         img_name = r.json()["claims"]["P18"][0]["mainsnak"]["datavalue"]["value"]
         img_source = r.json()["claims"]["P18"][0]["mainsnak"]["datatype"]
